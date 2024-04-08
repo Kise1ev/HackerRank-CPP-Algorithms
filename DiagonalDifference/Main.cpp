7f4ba1a65f7371f1b1a93cb3852b6d17a1d886f0
Roman Kiselev <147641616+Kise1ev@users.noreply.github.com>
2024-04-08 22:29:49 +0300

    Update Main.cpp

--- DiagonalDifference/Main.cpp
+++ DiagonalDifference/Main.cpp
@@ -8,17 +8,16 @@ int main() {
 
     vector<vector<int>> array(n, vector<int>(n));
     cout << "Input " << n << " numbers separated by a space: " << endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++) 
             cin >> array[i][j];
-        }
-    }
 
     int rightSum = 0, leftSum = 0;
     for (int i = 0; i < array.size(); i++) {
         rightSum += array[i][i];
         leftSum += array[i][array.size() - 1 - i];
     }
+    
     int result = abs(rightSum - leftSum);
     cout << "Result: " << result << endl << endl;
 
